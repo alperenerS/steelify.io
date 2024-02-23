f556ab61bce3b66f6f9dd54f3c0d6250ba8cdcf5
bbalbayrak <b.b.albayrak@hotmail.com>
2024-02-23 13:46:14 +0300

    User updateOwnData Function Done

--- server/src/modules/user/user.controller.ts
+++ server/src/modules/user/user.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpStatus,
+  Put,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtGuard } from '../auth/guard/jwt.guard';
+import { UserService } from './user.service';
+import { Request, Response } from 'express';
+import { UserDto } from './dto/user.dto';
 
+@UseGuards(JwtGuard)
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Put('updateData')
+  async updateOwnData(
+    @Body() user: UserDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const token = req.headers.authorization.split(' ')[1];
+    const response = await this.userService.updateOwnData(user, token);
+
+    return res
+      .status(HttpStatus.ACCEPTED)
+      .json({ message: 'Successfully created !', response: response });
+  }
+}
