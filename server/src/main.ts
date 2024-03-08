ac90f3728416cfa58e53e0671b29f869744e35c8
emremataraci <mtrcemre@gmail.com>
2024-03-08 05:18:06 +0300

    enable CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,8 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-    whitelist:true
-  }))
+    whitelist: true,
+  }));
+
+  app.enableCors();
+
   await app.listen(3001);
 }
 bootstrap();
