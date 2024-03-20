6ff42b88e85c7c3d2bbd256dcf3e3dbe2866cae2
emremataraci <mtrcemre@gmail.com>
2024-03-20 21:39:43 +0300

    Future: Get Quote Form Unnecassary Command Lines Removed

--- client/src/Pages/GetQuote/getQuoteForm.js
+++ client/src/Pages/GetQuote/getQuoteForm.js
@@ -1,20 +1,18 @@
 import React, { useState } from 'react';
 import { Form, Input, Button, Upload, message } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
-import FileUpload from './fileUpload'; // Lütfen doğru import yolu kullandığınızdan emin olun
+import FileUpload from './fileUpload';
 
 const GetQuoteForm = ({ onSubmit }) => {
     const [form] = Form.useForm();
-    const [fileList, setFileList] = useState([]); // Dosya listesi için state
-    const [photoList, setPhotoList] = useState([]); // Fotoğraf listesi için state
+    const [fileList, setFileList] = useState([]);
+    const [photoList, setPhotoList] = useState([]);
 
-    // FileUpload komponentinden gelen dosyaları tutan fonksiyon
     const handleFileChange = (newFileList) => {
       setFileList(newFileList);
   };
   
 
-    // Upload komponentinden gelen fotoğrafları tutan fonksiyon
     const handlePhotoChange = ({ fileList: newPhotoList }) => {
         const newPhotos = newPhotoList.map(file => 
             (file.originFileObj ? file.originFileObj : file)
@@ -22,9 +20,7 @@ const GetQuoteForm = ({ onSubmit }) => {
         setPhotoList(newPhotos);
     };
 
-    // Form başarıyla gönderildiğinde çalışacak fonksiyon
     const onFinish = (values) => {
-        // onSubmit fonksiyonunu çağırarak üst komponente form verilerini ve dosya listelerini gönder
         onSubmit(values, fileList, photoList);
     };
 
@@ -65,7 +61,7 @@ const GetQuoteForm = ({ onSubmit }) => {
                     name="samplePhotos"
                     listType="picture"
                     multiple={true}
-                    beforeUpload={() => false} // Dosya yükleme işlemini engeller
+                    beforeUpload={() => false}
                     onChange={handlePhotoChange}
                 >
                     <Button icon={<UploadOutlined />}>Click to upload</Button>
