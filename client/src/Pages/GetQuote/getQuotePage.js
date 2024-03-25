8a93da6f7cbd7d879710ca24fd597e8ed9f20060
emremataraci <mtrcemre@gmail.com>
2024-03-25 15:01:31 +0100

    Future: Navigate to Dynamic url from getquote to request-details

--- client/src/Pages/GetQuote/getQuotePage.js
+++ client/src/Pages/GetQuote/getQuotePage.js
@@ -3,11 +3,14 @@ import { Layout, Typography, Card, Row, Col, message } from "antd";
 import GetQuoteForm from './getQuoteForm';
 import axios from 'axios';
 import { API_BASE_URL } from '../../config';
+import { useNavigate } from 'react-router-dom';
 
 const { Header, Content } = Layout;
 const { Title } = Typography;
 
 const GetQuotePage = () => {
+  const navigate = useNavigate();
+
   const handleSubmit = async (values, fileList, photoList) => {
     const token = localStorage.getItem('accessToken');
     const formData = new FormData();
@@ -34,31 +37,34 @@ const GetQuotePage = () => {
 
         if (response.data.success) {
             message.success('Quote request submitted successfully!');
+            //if the request success, navigate to the dynamic order id page
+            navigate(`/request-details/${response.data.order_id}`);
         } else {
             message.error('Failed to submit quote request.');
         }
     } catch (error) {
         message.error(`Error submitting quote request: ${error.message}`);
     }
-};
-    return (
-      <Layout style={{ backgroundColor: "#fff" }}>
-      <Header style={{ backgroundColor: "#fff" }}>
-        <Title level={2} style={{ color: "#000", textAlign: "center" }}>
-          Get Quote
-        </Title>
-      </Header>
-      <Content style={{ padding: "50px", backgroundColor: "#fff" }}>
-        <Row justify="center">
-          <Col xs={24} sm={20} md={16} lg={12} xl={10}>
-            <Card bordered={false} style={{ padding: "20px" }}>
-              <GetQuoteForm onSubmit={handleSubmit} />
-            </Card>
-          </Col>
-        </Row>
-      </Content>
-    </Layout>
-    );
+  };
+
+  return (
+    <Layout style={{ backgroundColor: "#fff" }}>
+    <Header style={{ backgroundColor: "#fff" }}>
+      <Title level={2} style={{ color: "#000", textAlign: "center" }}>
+        Get Quote
+      </Title>
+    </Header>
+    <Content style={{ padding: "50px", backgroundColor: "#fff" }}>
+      <Row justify="center">
+        <Col xs={24} sm={20} md={16} lg={12} xl={10}>
+          <Card bordered={false} style={{ padding: "20px" }}>
+            <GetQuoteForm onSubmit={handleSubmit} />
+          </Card>
+        </Col>
+      </Row>
+    </Content>
+  </Layout>
+  );
 };
 
 export default GetQuotePage;
