ab1aadaccd4af493b855fe77a539572ebd81697d
emremataraci <mtrcemre@gmail.com>
2024-04-01 22:06:41 +0300

    Future: Save button part1

--- client/src/Pages/RequestDetails/Components/saveButton.js
+++ client/src/Pages/RequestDetails/Components/saveButton.js
@@ -1,13 +1,55 @@
-import React from 'react';
-import { Button } from 'antd';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { Button, notification } from "antd";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { API_BASE_URL } from "../../../config";
 
-const SaveButton = () => {
+const SaveButton = ({ orderData }) => {
   const navigate = useNavigate();
+  const { order_id } = useParams();
 
-  const handleSave = () => {
-    navigate('/my-orders');
+  const handleSave = async () => {
+    if (!order_id) {
+      notification.error({
+        message: "Error",
+        description: "Order ID is missing.",
+      });
+      return;
+    }
+  
+    // 'accessToken' adıyla kaydedilen token'ı localStorage'dan al
+    const accessToken = localStorage.getItem('accessToken');
+  
+    try {
+      // `orderData` içinde güncellenmesi gereken sipariş bilgileri olmalıdır.
+      const response = await axios.put(
+        `${API_BASE_URL}/order/updateOrder/${order_id}`,
+        orderData,
+        {
+          headers: {
+            // Bearer token ile Authorization başlığını ekleyin
+            'Authorization': `Bearer ${accessToken}`
+          }
+        }
+      );
+  
+      if (response.status === 200) {
+        notification.success({
+          message: "Success",
+          description: "Order updated successfully!",
+        });
+        navigate("/my-requests");
+      } else {
+        throw new Error("Order update failed");
+      }
+    } catch (error) {
+      notification.error({
+        message: "Error",
+        description: `Order update failed. ${error.message}`,
+      });
+    }
   };
+  
 
   return (
     <Button type="primary" onClick={handleSave}>
