2c7644c31bdf429b51a8e330a79261c96af12367
emremataraci <mtrcemre@gmail.com>
2024-04-05 14:10:13 +0300

    Future: Footer Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Layout } from 'antd';
 import Login from './Pages/Login/login';
 import Register from './Pages/Register/register';
 import LandingPage from './Pages/LandingPage/landingPage';
@@ -12,27 +13,30 @@ import MyRequests from './Pages/MyRequests/myRequests';
 import Profile from './Pages/Profile/profile';
 import PrivateRoute from './Utils/PrivateRoute/privateRoute';
 import AppFooter from './Pages/Footer/footer';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <div>
+      <Layout className="site-layout">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/landing-page" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/request-details/:order_id" element={<PrivateRoute><RequestDetails /></PrivateRoute>} />
-          <Route path="/get-quote" element={<PrivateRoute><GetQuotePage /></PrivateRoute>} />
-          <Route path="/get-quote/:order_id" element={<PrivateRoute><GetQuotePage /></PrivateRoute>} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/my-requests" element={<PrivateRoute><MyRequests /></PrivateRoute>} />
-          <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
-          <Route path="/*" element={<PageNotFound />} />
-        </Routes>
+        <Layout.Content className="site-layout-content">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/landing-page" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/request-details/:order_id" element={<PrivateRoute><RequestDetails /></PrivateRoute>} />
+            <Route path="/get-quote" element={<PrivateRoute><GetQuotePage /></PrivateRoute>} />
+            <Route path="/get-quote/:order_id" element={<PrivateRoute><GetQuotePage /></PrivateRoute>} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/my-requests" element={<PrivateRoute><MyRequests /></PrivateRoute>} />
+            <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+            <Route path="/*" element={<PageNotFound />} />
+          </Routes>
+        </Layout.Content>
         <AppFooter />
-      </div>
+      </Layout>
     </Router>
   );
 }
