b48eed02a48b12d706189a580e30abcb35c93c07
emremataraci <mtrcemre@gmail.com>
2024-04-15 05:23:19 +0300

    Future: Token Expiration & Navigate Update

--- client/src/Utils/Auth/authService.js
+++ client/src/Utils/Auth/authService.js
@@ -1,39 +1,52 @@
 export const saveUserInfo = ({ data }) => {
   const { access_token, data: userData } = data;
-  localStorage.setItem('accessToken', access_token);
-  localStorage.setItem('userInfo', JSON.stringify(userData));
+  localStorage.setItem("accessToken", access_token);
+  localStorage.setItem("userInfo", JSON.stringify(userData));
 };
 
 export const getUserInfo = () => {
-  const userInfoStr = localStorage.getItem('userInfo');
+  const userInfoStr = localStorage.getItem("userInfo");
   try {
     return userInfoStr ? JSON.parse(userInfoStr) : null;
   } catch (error) {
-    console.error('getUserInfo parsing error:', error);
+    console.error("getUserInfo parsing error:", error);
     clearUserInfo();
     return null;
   }
 };
 
 export const clearUserInfo = () => {
-  localStorage.removeItem('accessToken');
-  localStorage.removeItem('userInfo');
+  localStorage.removeItem("accessToken");
+  localStorage.removeItem("userInfo");
 };
 
-// export const checkTokenExpiry = () => {
-//   const accessToken = localStorage.getItem('accessToken');
-//   if (!accessToken) {
-//     clearUserInfo();
-//     return false;
-//   }
+export const checkTokenExpiry = () => {
+  const accessToken = localStorage.getItem("accessToken");
 
-//   const { exp } = JSON.parse(atob(accessToken.split('.')[1]));
-//   const now = Date.now() / 1000;
+  if (!accessToken) {
+    clearUserInfo();
+    return false;
+  }
 
-//   if (exp < now) {
-//     clearUserInfo();
-//     return false;
-//   }
+  try {
+    const splitToken = accessToken.split(".");
+    if (splitToken.length !== 3) {
+      throw new Error("Token format invalid");
+    }
 
-//   return true;
-// };
+    const { exp } = JSON.parse(atob(splitToken[1]));
+
+    const now = Date.now() / 1000;
+
+    if (exp < now) {
+      clearUserInfo();
+      return false;
+    }
+
+    return true;
+  } catch (error) {
+    console.error("Token error:", error);
+    clearUserInfo();
+    return false;
+  }
+};
--- client/src/Utils/PrivateRoute/privateRoute.js
+++ client/src/Utils/PrivateRoute/privateRoute.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-
-const isAuthenticated = () => {
-  const token = localStorage.getItem('accessToken');
-  return !!token;
-};
+import { checkTokenExpiry } from '../Auth/authService';
 
 const PrivateRoute = ({ children }) => {
-  return isAuthenticated() ? children : <Navigate to="/login" replace />;
+  const isTokenValid = checkTokenExpiry();
+
+  return isTokenValid ? children : <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
