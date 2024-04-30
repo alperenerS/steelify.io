30b72fb3bfd6672c8b7465398b729d6c63cbe568
emremataraci <mtrcemre@gmail.com>
2024-04-30 03:54:13 +0300

    Future: Mail part1

---
+++ client/src/EmailTemplates/ForgotPassword/forgotPasswordMailTemplate.js
@@ -0,0 +1,34 @@
+const getEmailHtml = (userFirstname, token) => `
+<html>
+  <body style="background-color: #f6f9fc; padding: 10px 0;">
+    <div style="background-color: #ffffff; border: 1px solid #f0f0f0; padding: 45px;">
+      <img src="https://yenastorage.blob.core.windows.net/steelify/steelifyLogo.png" width="180" height="60" alt="Steelify Logo" style="display: block; margin: 0 auto;" />
+      <div>
+        <p style="font-size: 16px; font-family: 'Open Sans', 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-weight: 300; color: #404040; line-height: 26px;">
+          Hi ${userFirstname},
+        </p>
+        <p style="font-size: 16px; font-family: 'Open Sans', 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-weight: 300; color: #404040; line-height: 26px;">
+          We received a request to reset the password for your Steelify.io account. If you made this request, please click the button below to set a new password.
+          <a href="http://localhost:3000/reset-password/${token}" style="background-color: #007ee6; border-radius: 4px; color: #fff; font-family: 'Open Sans', 'Helvetica Neue', Arial; font-size: 15px; text-decoration: none; text-align: center; display: block; width: 210px; padding: 14px 7px; margin-top: 20px;">
+            Reset Password
+          </a>
+        </p>
+        <p style="font-size: 16px; font-family: 'Open Sans', 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-weight: 300; color: #404040; line-height: 26px;">
+          If you did not request a password reset or do not wish to change your password, you may safely disregard and delete this email.
+        </p>
+        <p style="font-size: 16px; font-family: 'Open Sans', 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-weight: 300; color: #404040; line-height: 26px;">
+          Thank you for choosing Steelify!
+        </p>
+        <p style="font-size: 16px; font-family: 'Open Sans', 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-weight: 300; color: #404040; line-height: 26px;">
+          Best regards,
+        </p>
+        <p style="font-size: 16px; font-family: 'Open Sans', 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-weight: 300; color: #404040; line-height: 26px;">
+          Steelify Team
+        </p>
+      </div>
+    </div>
+  </body>
+</html>
+`;
+
+export default getEmailHtml;
--- client/src/Pages/ForgotPassword/forgotPassword.js
+++ client/src/Pages/ForgotPassword/forgotPassword.js
@@ -1,48 +1,98 @@
 import React, { useState } from 'react';
-import { Form, Input, Button, Typography, Card } from 'antd';
-import './forgotPassword.css'; // Ensure the CSS file is imported correctly
-
-const { Title } = Typography;
+import { Form, Input, Button, Card, Result } from 'antd';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import './forgotPassword.css';
+import { API_BASE_URL } from '../../config';
+import getEmailHtml from '../../EmailTemplates/ForgotPassword/forgotPasswordMailTemplate';
 
 const ForgotPassword = () => {
-    const [email, setEmail] = useState('');
+  const [email, setEmail] = useState('');
+  const [result, setResult] = useState(null);
+  const navigate = useNavigate();
 
-    const handleSubmit = () => {
-        console.log('Sending password reset link to:', email);
-        // An API call would be made here to send the reset link
-    };
+  const handleSubmit = async () => {
+    const userFirstname = email.substring(0, email.indexOf("@")); // "@" işaretine kadar olan kısmı alın
 
-    return (
-        <div className="forgot-password-container">
-            <Card title="Reset Your Password" className="forgot-password-card">
-                <Form
-                    name="forgot_password"
-                    onFinish={handleSubmit}
-                    className="forgot-password-form"
-                    layout="vertical"
-                >
-                    <Form.Item
-                        label="Email"
-                        name="email"
-                        rules={[
-                            { required: true, message: 'Please enter your email address.' },
-                            { type: 'email', message: 'Please enter a valid email address!' }
-                        ]}
-                    >
-                        <Input
-                            placeholder="Enter your email"
-                            onChange={e => setEmail(e.target.value)}
-                        />
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" className="forgot-password-button">
-                            Send Reset Link
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Card>
-        </div>
-    );
+    try {
+      // E-posta içeriği için token olmadan önce hazırlanır
+      const placeholderToken = "placeholder-token"; // API'den gerçek token alınana kadar geçici bir token
+      const initialHtml = getEmailHtml(userFirstname, placeholderToken);
+
+      // Token almak ve e-posta göndermek için API çağrısı yapılıyor
+      const response = await axios.post(`${API_BASE_URL}/email-sender/reset-password`, {
+        to: email,
+        subject: 'Password Reset Request',
+        html: initialHtml.replace(placeholderToken, "${token}")  // Gerçek token ile placeholder token değiştiriliyor
+      });
+
+      if (response.data && response.data.message.includes("Successfully")) {
+        setResult({
+          status: 'success',
+          title: 'Password Reset Link Sent',
+          subTitle: 'A link to reset your password has been sent to your email.'
+        });
+      } else {
+        throw new Error('Failed to send email');
+      }
+    } catch (error) {
+      console.error('Error during the email sending process:', error);
+      setResult({
+        status: 'error',
+        title: 'Failed to Send Reset Email',
+        subTitle: 'There was an issue sending your password reset email. Please try again.'
+      });
+    }
+  };
+
+  const handleBackHome = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className="forgot-password-container">
+      {!result ? (
+        <Card title="Reset Your Password" className="forgot-password-card">
+          <Form
+            name="forgot_password"
+            onFinish={handleSubmit}
+            className="forgot-password-form"
+            layout="vertical"
+          >
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                { required: true, message: 'Please enter your email address.' },
+                { type: 'email', message: 'Please enter a valid email address!' }
+              ]}
+            >
+              <Input
+                placeholder="Enter your email"
+                onChange={e => setEmail(e.target.value)}
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="forgot-password-button">
+                Send Reset Link
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      ) : (
+        <Result
+          status={result.status}
+          title={result.title}
+          subTitle={result.subTitle}
+          extra={[
+            <Button type="primary" key="console" onClick={handleBackHome}>
+              Return to Homepage
+            </Button>
+          ]}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ForgotPassword;
