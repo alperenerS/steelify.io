980a1b1ee35e59fc9311212a62539d184264c88d
bbalbayrak <b.b.albayrak@hotmail.com>
2024-05-02 13:14:07 +0300

    Email CreateToken Fixed

--- server/src/modules/email-sender/email-sender.controller.ts
+++ server/src/modules/email-sender/email-sender.controller.ts
@@ -19,11 +19,15 @@ export class EmailSenderController {
 
   @Get('create-token')
   async createToken(@Req() req: Request, @Res() res: Response) {
+    const { email } = req.body;
     const token = await this.emailService.generatePasswordResetToken();
+    const userName = await this.emailService.findUserByEmail(email);
 
-    return res
-      .status(HttpStatus.OK)
-      .json({ message: 'Token Successfully Created !', data: token });
+    return res.status(HttpStatus.OK).json({
+      message: 'Token Successfully Created !',
+      token: token,
+      username: userName.name,
+    });
   }
 
   @Post('reset-password')
