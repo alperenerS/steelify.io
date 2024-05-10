3c5f17a992d4c8e179f730f626349d6ef774127a
emremataraci <mtrcemre@gmail.com>
2024-05-10 13:42:46 +0300

    Future: Capabilities Title added

--- client/src/Pages/LandingPage/capabilities.js
+++ client/src/Pages/LandingPage/capabilities.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Card, Col, Row } from 'antd';
-import './capabilities.css';
+import { Card, Col, Row } from "antd";
+import "./capabilities.css";
 
 import steelifyLaserCutting from "./assets/steelifyLaserCutting.png";
 import steelifyBending from "./assets/steelifyBending.png";
@@ -19,15 +19,33 @@ const cardData = [
 const CapabilitiesSection = () => {
   return (
     <div className="capabilities-wrapper">
+      <h1 style={{ textAlign: "center" }}>Capabilities</h1>
       <Row gutter={[16, 16]} justify="center">
         {cardData.map((card, index) => (
-          <Col key={index} xs={24} sm={12} md={8} lg={6} xl={4} style={{ maxWidth: '300px' }}>
+          <Col
+            key={index}
+            xs={24}
+            sm={12}
+            md={8}
+            lg={6}
+            xl={4}
+            style={{ maxWidth: "300px" }}
+          >
             <Card
               hoverable
               className="custom-card"
-              cover={<img alt={card.title} src={card.image} className="custom-card-image" />}
+              cover={
+                <img
+                  alt={card.title}
+                  src={card.image}
+                  className="custom-card-image"
+                />
+              }
             >
-              <Card.Meta title={<div style={{ whiteSpace: 'normal' }}>{card.title}</div>} className="custom-card-title" />
+              <Card.Meta
+                title={<div style={{ whiteSpace: "normal" }}>{card.title}</div>}
+                className="custom-card-title"
+              />
             </Card>
           </Col>
         ))}
