99df332744f2c31101a2f127d6580579a001c9b1
bbalbayrak <b.b.albayrak@hotmail.com>
2024-05-17 15:58:18 +0300

    lowercase Email middleware is done

--- server/src/modules/auth/dto/auth.dto.ts
+++ server/src/modules/auth/dto/auth.dto.ts
@@ -1,8 +1,10 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ValidateEmail } from 'src/modules/utils/emailLowercase';
 
 export class AuthDto {
   @IsEmail()
   @IsNotEmpty()
+  @ValidateEmail()
   readonly email: string;
   @IsString()
   @IsNotEmpty()
