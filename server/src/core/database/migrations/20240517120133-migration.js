67849499e03af523ad37b053fe899d6c0976ba82
bbalbayrak <b.b.albayrak@hotmail.com>
2024-05-17 15:05:15 +0300

    sequelize migration done

--- server/src/core/database/migrations/20240517120133-migration.js
+++ server/src/core/database/migrations/20240517120133-migration.js
@@ -3,19 +3,19 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('Addresses', 'order_id', {
-      type: Sequelize.INTEGER,
-      allowNull: true, // veya false, gereksinimlerinize göre ayarlayın
+    return queryInterface.addColumn('Users', {/*eklenecek sutun adi */}, {
+      type: Sequelize.STRING,
+      allowNull: true,
       references: {
         model: 'Orders', // Bağlanacak tablo adı
         key: 'id',
-      },
+      }, //Foreign Key
       onUpdate: 'CASCADE',
       onDelete: 'SET NULL',
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('Addresses', 'order_id');
+    return queryInterface.removeColumn('Users', {/*eklenecek sutun adi */});
   },
 };
