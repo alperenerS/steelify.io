47b3bb57c6a77934a9870287778f2b74409c2ee3
emremataraci <mtrcemre@gmail.com>
2024-06-01 09:56:20 +0300

    Feature: Forgot Password img link updated

--- client/src/EmailTemplates/ForgotPassword/forgotPasswordMailTemplate.js
+++ client/src/EmailTemplates/ForgotPassword/forgotPasswordMailTemplate.js
@@ -2,7 +2,7 @@ const getEmailHtml = (username, token, CLIENT_BASE_URL) => `
 <html>
   <body style="background-color: #f6f9fc; padding: 10px 0;">
     <div style="background-color: #ffffff; border: 1px solid #f0f0f0; padding: 45px;">
-      <img src="https://yenastorage.blob.core.windows.net/steelify/steelifyLogo.png" width="180" height="60" alt="Steelify Logo" style="display: block; margin: 0 auto;" />
+      <img src="https://yenastorage.blob.core.windows.net/steelify/steelify_logo.png" width="180" height="60" alt="Steelify Logo" style="display: block; margin: 0 auto;" />
       <div>
         <p style="font-size: 16px; font-family: 'Open Sans', 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-weight: 300; color: #404040; line-height: 26px;">
           Hi ${username},
