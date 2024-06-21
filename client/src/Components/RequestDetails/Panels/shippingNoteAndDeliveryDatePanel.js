0250efbca7f58e1facec202156c9841d48991483
emremataraci <mtrcemre@gmail.com>
2024-06-22 02:40:42 +0300

    Bugfix: Shipping Note removed

--- client/src/Components/RequestDetails/Panels/shippingNoteAndDeliveryDatePanel.js
+++ client/src/Components/RequestDetails/Panels/shippingNoteAndDeliveryDatePanel.js
@@ -4,12 +4,6 @@ import { Form, DatePicker, Input } from "antd";
 const ShippingNoteAndDeliveryDatePanel = ({ initialValues }) => {
   return (
     <>
-      <p>
-        We are currently unable to get rates from our shipping provider. If you
-        would like to proceed with checkout at this time, a member of our
-        support team will contact you to process a shipping charge after you
-        place your order.
-      </p>
       <Form.Item label="Delivery Date" name="deliveryDate">
         <DatePicker
           style={{ width: "100%" }}
