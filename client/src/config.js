30b7daa632d4a44c9cfa4eef298fdb87567d4b2d
emremataraci <mtrcemre@gmail.com>
2024-06-26 12:39:31 +0300

    Feature: Update Configjs & auth.service.ts (ODOO Base URL)

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL = 'http://localhost:3005/api';
 export const CLIENT_BASE_URL = 'http://localhost:3000';
-export const ODOO_API_BASE_URL = 'https://portal-steelify-steelify-api-13738619.dev.odoo.com/api';
+export const ODOO_API_BASE_URL = 'https://portal-steelify-staging-13657712.dev.odoo.com/api';
 
 // https://steelify.io:3003/api
